12885e3d4590f6c1e9c1733135d66d02da17fc5d
mjackson <mjackson@mail.pima.edu>
2017-10-15 16:31:51 -0700

    Got the state machine working.
    How it works:
    Actions:
    1. Create a script that extends Action
    2. Add: [CreateAssetMenu(menuName = "StateMachine/Actions/NAMEOFACTION")] above the class name
    3. Implement Act function.
    4. Inside the Act function it should call another function that does the action like patrol
    
    Next you need a Decision
    5. Create a script that extends Decisions
    6. Add: [CreateAssetMenu(menuName = "StateMachine/Decisions/NAMEOFDECISION")] above the class name
    7. In the Decide function have to call another function that returns a bool. This second function will be where it decides if it will transition or not.
    
    8. In the Actions folder right click
    9. Hover over StateMachine
    10. Hover over Actions
    11. Select [NAMEGOESHERE]Action
    12. Name it PatrolAction
    13. In the Decisions folder right click
    14. Hover over StateMachine
    15. Hove over Decisions
    16. Name it [NAMEGOESHERE]Decisions
    17. In States folder right click
    18. Hover over StateMachine
    19. Click States
    20. Name it [NAMEGOESHERE]
    21. In the State change the number of actions to 1
    22. Drag and drop the action into the slot
    23. For Transitions change the number to 1
    24. Drag and drop a decision, true state, and a false state.
    25. In the StateController on the prefab drag and drop the current state (this is what will be the start state), and the RemainInState (should already be created).
    26. Fin!

---
+++ Assets/DiscordChatGame/Scripts/AI/StateMachine/ToPatrol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "StateMachine/Decisions/ToPatrol")]
+public class ToPatrol : Decision {
+
+    public override bool Decide(StateController controller)
+    {
+        return checkForTarget(controller);
+    }
+
+    //Currently not implemented, will need to check to see if the player is visiable or not
+    private bool checkForTarget(StateController controller)
+    {
+        return false;
+    }
+
+}
