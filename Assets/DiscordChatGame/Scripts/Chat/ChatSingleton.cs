3b80eacc2ed541010dad354c4be6634412c29771
unknown <andrewseward93@gmail.com>
2017-10-18 01:37:56 -0700

    updated DSharpPlus to github version.

---
+++ Assets/DiscordChatGame/Scripts/Chat/ChatSingleton.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatSingleton : MonoBehaviour
+{
+    public static IChatActor ChatActor;
+}
---
+++ Assets/DiscordChatGame/Scripts/Chat/Discord/DiscordChatActor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class DiscordChatActor : MonoBehaviour, IChatActor
+{
+    public async Task Run(string token)
+    {
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
