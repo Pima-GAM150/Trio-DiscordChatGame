69b77281fc196f50dd9bea6e6e5de3786357f6d2
mjackson34 <mjackson34@mail.pima.edu>
2017-10-26 19:38:05 -0700

    Started working on new game
    basic player
    Some enemy stuff

---
+++ Assets/DiscordChatGame/Scripts/Player/Skill Tree/PlayerSkills.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSkills : MonoBehaviour {
+
+    public List<Skill> currentSkills;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/DiscordChatGame/Scripts/Player/Skill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+    public string name;
+    public bool melee;
+    public Dictionary<string, float> modifier;
+    public List<string> nextSkill;
+
+}
